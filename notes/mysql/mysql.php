1a79e20e84f12df9c6fc12ddde242d4070c4af82
wangkeji <602823863@qq.com>
2017-12-19 15:29:29 +0800

    added 添加mysql

---
+++ notes/mysql/mysql.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author     :wkj
+ * @createTime :2017/12/19 15:28
+ * @description:
+ */
+根据日期分组 时间戳字段
+
+SELECT DATE_FORMAT( FROM_UNIXTIME( add_time ) ,  '%Y-%m-%d' ) AS date, COUNT( * ) AS total
+FROM  `tableName`
+WHERE add_time >=1509465600
+AND add_time <1512057600
+GROUP BY DATE_FORMAT( FROM_UNIXTIME( add_time ) ,  '%Y-%m-%d' )
